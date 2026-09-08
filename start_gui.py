7bd486cbc0da792a1bdd6fb36a4db5eb834fc149
Fredy Luis Vidalón Lozano <118131613+tec-2022@users.noreply.github.com>
2026-09-08 16:22:01 -0700

    feat: add desktop GUI launcher

---
+++ start_gui.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Launch the Academic Automation Kit desktop app.
+Created by Fredy Luis Vidalón Lozano.
+"""
+from academic_toolkit.desktop_app import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
